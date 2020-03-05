6677c63a94675330722d3cd0d59e1fcf3af14bbb
Himanshu Shukla <acmehimanshu@gmail.com>
2020-03-05 16:18:19 +0530

    singleton design pattern example

---
+++ src/com/designpattern/singleton/DoubleCheckedLockingSingleton.java
@@ -0,0 +1,19 @@
+package com.designpattern.singleton;
+
+public class DoubleCheckedLockingSingleton {
+	DoubleCheckedLockingSingleton doublecheckedSingleton;
+	
+	private DoubleCheckedLockingSingleton() {}
+	
+	public DoubleCheckedLockingSingleton getInstance()
+	{
+		if(doublecheckedSingleton==null)
+		{
+			synchronized (DoubleCheckedLockingSingleton.class) {
+				if(doublecheckedSingleton==null)
+					doublecheckedSingleton=new DoubleCheckedLockingSingleton();
+			}
+		}		
+		return doublecheckedSingleton;
+	}
+}
---
+++ src/com/designpattern/singleton/SimpleSingleton.java
@@ -0,0 +1,14 @@
+package com.designpattern.singleton;
+
+public class SimpleSingleton {
+	SimpleSingleton singletonObject;
+	private SimpleSingleton() {}
+	
+	public SimpleSingleton getSimpleSingletonInstance()
+	{
+		if(singletonObject==null)
+			singletonObject=new SimpleSingleton();
+		
+		return singletonObject;
+	}
+}
